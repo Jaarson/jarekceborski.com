73780b46c78736ef05cf7d48f82f6af0417e3f80
Jaroslaw Ceborski <jaroslawceborski@MacBook-Pro-Jarek.local>
2019-10-06 10:49:58 +0200

    Home page in index.js, other using templates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+import { graphql, StaticQuery } from "gatsby"
+
+const HomePage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <StaticQuery
+      query={graphql`
+        {
+          wordpressPage(slug: { eq: "home" }) {
+            title
+            content
+          }
+        }
+      `}
+      render={props => (
+        <div>
+          <h1>{props.wordpressPage.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{ __html: props.wordpressPage.content }}
+          />
+        </div>
+      )}
+    />
   </Layout>
 )
 
-export default IndexPage
+export default HomePage
